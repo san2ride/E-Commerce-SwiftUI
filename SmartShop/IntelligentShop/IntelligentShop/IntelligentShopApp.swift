f0517fd36fd685855c98397eab826dc6f6f96976
Jason <sanchezjason@me.com>
2025-01-12 15:11:11 -0700

    install express

---
+++ SmartShop/IntelligentShop/IntelligentShop/IntelligentShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntelligentShopApp.swift
+//  IntelligentShop
+//
+//  Created by Jason Sanchez on 1/12/25.
+//
+
+import SwiftUI
+
+@main
+struct IntelligentShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
