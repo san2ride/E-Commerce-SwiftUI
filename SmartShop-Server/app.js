66146c9dbf4b2cec5c145f9403cd5bc547f37bcd
Jason <sanchezjason@me.com>
2025-01-13 15:03:04 -0700

    smart_shop_server/ add app.get

--- SmartShop-Server/app.js
+++ SmartShop-Server/app.js
@@ -1,6 +1,10 @@
-const express = require( 'express')
-const app = express ()
-console. log(app)
+const express = require('express')
+const app = express()
+
+app.get("/", (req, res) => {
+    res.send("Hello Jason")
+})
+
 
 // start the server
 app.listen(8080, () => {
