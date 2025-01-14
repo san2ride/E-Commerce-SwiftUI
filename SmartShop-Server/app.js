ccc97e44717c05f86c93beb18e2e3cbdaa734303
Jason <sanchezjason@me.com>
2025-01-13 21:17:52 -0700

    smart_shop_server/ dynamic routes

--- SmartShop-Server/app.js
+++ SmartShop-Server/app.js
@@ -1,10 +1,23 @@
 const express = require('express')
 const app = express()
 
-app.get("/", (req, res) => {
-    res.send("Hello Jason")
+app.get('/', (req, res) => {
+    res.send('ROOT')
 })
 
+app.get('/automobile/:suv/year/:year', (req, res) => {
+    const suv = req.params.suv
+    const year = req.params.year
+    res.send(`Your ${year} ${suv} is here!`)
+})
+/*
+app.get('/suv/fourrunner', (req, res) => {
+    res.send('4Runners are cool')
+})
+app.get('/suv/defender', (req, res) => {
+    res.send('Defenders are dope!')
+})
+*/
 
 // start the server
 app.listen(8080, () => {
