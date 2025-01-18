304c5392deb46d978bd7eb344f7dca41139f7dc2
Jason <sanchezjason@me.com>
2025-01-17 19:29:40 -0700

    json routes

--- SmartShop-Server/app.js
+++ SmartShop-Server/app.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const app = express()
+const automobiles = [{brand: "Porsche", style: "sports car"}, {brand: "Defender", style: "suv"}]
 
 app.get('/', (req, res) => {
     res.send('ROOT')
+})
+
+app.get('/hello', (req, res) => {
+    res.json({ message: 'Hello Jason'})
+})
+
+app.get('/automobile', (req, res) => {
+    res.json(automobile)
 })
 
 app.get('/automobile/:suv/year/:year', (req, res) => {
@@ -10,6 +19,12 @@ app.get('/automobile/:suv/year/:year', (req, res) => {
     const year = req.params.year
     res.send(`Your ${year} ${suv} is here!`)
 })
+
+app.get('/automobile/:style', (req, res) => {
+    const style = req.params.style
+    res.json(automobiles.filter(automobile => automobile.style.toLowerCase() == style.toLowerCase()))
+})
+
 /*
 app.get('/suv/fourrunner', (req, res) => {
     res.send('4Runners are cool')
