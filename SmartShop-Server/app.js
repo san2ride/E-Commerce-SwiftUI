00b28e397195f1c2b63b1eb223eda3004ee2e051
Jason <sanchezjason@me.com>
2025-01-21 13:45:40 -0700

    server

--- SmartShop-Server/app.js
+++ SmartShop-Server/app.js
@@ -1,43 +1,20 @@
 const express = require('express')
+const models = require('./models')
 const app = express()
-const automobiles = [{brand: "Porsche", style: "sports car"}, {brand: "Defender", style: "suv"}]
 
-app.get('/', (req, res) => {
-    res.send('ROOT')
-})
+// JSON parser
+app.use(express.json())
 
-app.get('/hello', (req, res) => {
-    res.json({ message: 'Hello Jason'})
-})
+app.post('/register', (req, res) => {
+    const { username, password } = req.body
 
-app.get('/automobile', (req, res) => {
-    res.json(automobile)
+    // create User
+    const newUser = models.User.create({
+        username: username,
+        password: password
+    })
+    res.status(201).json({success: true})
 })
-
-app.post('/automobile', (req, res) => {
-    const { brand, style } = req.body
-    res.send ('OK')
-})
-
-app.get('/automobile/:suv/year/:year', (req, res) => {
-    const suv = req.params.suv
-    const year = req.params.year
-    res.send(`Your ${year} ${suv} is here!`)
-})
-
-app.get('/automobile/:style', (req, res) => {
-    const style = req.params.style
-    res.json(automobiles.filter(automobile => automobile.style.toLowerCase() == style.toLowerCase()))
-})
-
-/*
-app.get('/suv/fourrunner', (req, res) => {
-    res.send('4Runners are cool')
-})
-app.get('/suv/defender', (req, res) => {
-    res.send('Defenders are dope!')
-})
-*/
 
 // start the server
 app.listen(8080, () => {
