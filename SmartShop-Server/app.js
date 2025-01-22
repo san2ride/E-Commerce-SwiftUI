4b28b60b284f78645c3d2cab8c8f97a3c9c09255
Jason <sanchezjason@me.com>
2025-01-21 17:11:20 -0700

    try catch existingUser

--- SmartShop-Server/app.js
+++ SmartShop-Server/app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const models = require('./models')
-const { body, validationResult } = require( 'express-validator');
+const { Op } = require('sequelize');
+const { body, validationResult } = require('express-validator');
+const { where } = require('sequelize');
 const app = express()
 
 // JSON parser
@@ -11,22 +13,35 @@ const registerValidator = [
     body('password', 'password cannot be empty!').not().isEmpty()
 ]
 
-app.post('/register', registerValidator, (req, res) => {
+app.post('/register', registerValidator, async (req, res) => {
     const errors = validationResult(req)
     console.log(errors)
-    if(!errors.isEmpty) {
+    if (!errors.isEmpty()) {
         const msg = errors.array().map(error => error.msg).join('')
-        return res.status(422).json({success: false, message: msg})
+        return res.status(422).json({ success: false, message: msg })
     }
-    
-    const { username, password } = req.body
 
-    // create User
-    const newUser = models.User.create({
-        username: username,
-        password: password
-    })
-    res.status(201).json({success: true})
+    try {
+        const { username, password } = req.body
+        const existingUser = await models.User.findOne({
+            where: {
+                username: { [Op.iLike]: username }
+            }
+        })
+
+        if (existingUser) {
+            return res.json({ message: 'Username taken!', success: false })
+        }
+
+        // create User
+        const newUser = models.User.create({
+            username: username,
+            password: password
+        })
+        res.status(201).json({ success: true })
+    } catch (error) {
+        res.status(500).json({ message: 'Internal server error.', success: false })
+    }
 })
 
 // start the server
