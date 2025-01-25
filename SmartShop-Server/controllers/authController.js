320ce14cff655856cf9db26e06f56087ee10c203
Jason <sanchezjason@me.com>
2025-01-25 15:25:45 -0700

    jsonwebtoken

--- SmartShop-Server/controllers/authController.js
+++ SmartShop-Server/controllers/authController.js
@@ -1,7 +1,36 @@
+const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs')
 const models = require('../models')
 const { Op } = require('sequelize');
 const { validationResult } = require('express-validator');
+
+exports.login = async (req, res) => {
+    const errors = validationResult(req)
+    console.log(errors)
+    if (!errors.isEmpty()) {
+        const msg = errors.array().map(error => error.msg).join('')
+        return res.status(422).json({ success: false, message: msg })
+    }
+    const { username, password } = req.body
+
+    // check if user exists
+    const existingUser = await models.User.findOne({
+        where: {
+            username: { [Op.iLike]: username }
+        }
+    })
+    if(!existingUser) {
+        return res.status(401).json({ message: 'Username or password is incorrect', success: false });
+    }
+    // check the password
+    const isPasswordValid = await bcrypt.compare(password, existingUser.password)
+    if(!isPasswordValid)
+        return res.status(401).json({ message: 'Password is incorrect', success: false });
+    // generate JWT token
+    const token = jwt.sign({ userId: existingUser.id}, 'SECRETKEY' {
+        expiresIn: '1h'
+    })
+}
 
 exports.register = async (req, res) => {
 
