87c89ce3cc7c729671eaeb489386677b921d3c85
Jason <sanchezjason@me.com>
2025-01-25 20:13:07 -0700

    url login func login

--- SmartShop/IntelligentShop/IntelligentShop/Utils/Constants.swift
+++ SmartShop/IntelligentShop/IntelligentShop/Utils/Constants.swift
@@ -10,5 +10,6 @@ import Foundation
 struct Constants {
     struct Urls {
         static let register: URL = URL(string: "http://localhost:8080/api/auth/register")!
+        static let login: URL = URL(string: "http://localhost:8080/api/auth/login")!
     }
 }
