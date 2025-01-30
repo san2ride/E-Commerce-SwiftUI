f2bf350eedf8bb5c434e4d976592ed4e5234579d
Jason <sanchezjason@me.com>
2025-01-30 14:32:24 -0700

    product url, loadAllProducts

--- SmartShop/IntelligentShop/IntelligentShop/Screens/ProductListScreen.swift
+++ SmartShop/IntelligentShop/IntelligentShop/Screens/ProductListScreen.swift
@@ -8,11 +8,23 @@
 import SwiftUI
 
 struct ProductListScreen: View {
+    @Environment(ProductStore.self) private var productStore
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List(productStore.products) { product in
+            Text(product.name)
+        }.task {
+            do {
+                try await productStore.loadAllProducts()
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
     }
 }
 
 #Preview {
-    ProductListScreen()
+    NavigationStack {
+        ProductListScreen()
+    }.environment(ProductStore(httpClient: .development))
 }
--- SmartShop/IntelligentShop/IntelligentShop/Stores/ProductStore.swift
+++ SmartShop/IntelligentShop/IntelligentShop/Stores/ProductStore.swift
@@ -11,8 +11,14 @@ import Observation
 @Observable
 class ProductStore {
     let httpClient: HTTPClient
+    private(set) var products: [Product] = []
     
     init(httpClient: HTTPClient) {
         self.httpClient = httpClient
+    }
+    
+    func loadAllProducts() async throws {
+        let resource = Resource(url: Constants.Urls.products, modelType: [Product].self)
+        products = try await httpClient.load(resource)
     }
 }
