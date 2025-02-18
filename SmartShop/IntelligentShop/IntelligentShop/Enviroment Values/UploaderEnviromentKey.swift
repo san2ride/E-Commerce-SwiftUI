06df21d78913902629b6dcf63048de7c4ab81058
Jason <sanchezjason@me.com>
2025-02-17 20:27:15 -0700

    ImagePicker

--- SmartShop/IntelligentShop/IntelligentShop/Enviroment Values/UploaderEnviromentKey.swift
+++ SmartShop/IntelligentShop/IntelligentShop/Enviroment Values/UploaderEnviromentKey.swift
@@ -18,4 +18,3 @@ extension EnvironmentValues {
         set { self[UploaderEnviromentKey.self] = newValue }
     }
 }
-
---
+++ SmartShop/IntelligentShop/IntelligentShop/Utils/ImagePicker.swift
@@ -0,0 +1,50 @@
+//
+//  ImagePicker.swift
+//  IntelligentShop
+//
+//  Created by Jason Sanchez on 2/17/25.
+//
+
+import Foundation
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    @Environment(\.dismiss) private var dismiss
+    typealias UIViewControllerType = UIImagePickerController
+    var sourceType: UIImagePickerController.SourceType = .camera
+    typealias Coordinator = ImagePickerCoordinator
+    @Binding var image: UIImage?
+    
+    func makeCoordinator() -> ImagePicker.Coordinator {
+        return ImagePickerCoordinator(self)
+    }
+    
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = false
+        picker.sourceType = sourceType
+        picker.delegate = context.coordinator
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+        
+    }
+    
+    class ImagePickerCoordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        var picker: ImagePicker
+        
+        init(_ picker: ImagePicker) {
+            self.picker = picker
+        }
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let uiImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+                self.picker.image = uiImage
+            }
+            self .picker.dismiss()
+        }
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            self.picker.dismiss()
+        }
+    }
+}
