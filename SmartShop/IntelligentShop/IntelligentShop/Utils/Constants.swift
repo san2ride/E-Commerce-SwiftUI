d06d5a47e827cb2edbc2c647d1ae35c4dfcc2e41
Jason <sanchezjason@me.com>
2025-02-21 21:36:48 -0700

    URL fix

--- SmartShop/IntelligentShop/IntelligentShop/Utils/Constants.swift
+++ SmartShop/IntelligentShop/IntelligentShop/Utils/Constants.swift
@@ -13,7 +13,7 @@ struct Constants {
         static let login: URL = URL(string: "http://localhost:8080/api/auth/login")!
         static let products: URL = URL(string: "http://localhost:8080/api/products")!
         static let createProducts: URL = URL(string: "http://localhost:8080/api/products")!
-        static let uploadProductImage: URL = URL(string: "http://localhost:8080/api/products/upload")!
+        static let uploadProductImage = URL(string: "http://localhost:8080/api/products/upload")!
         
         static func myProducts(_ userId: Int) -> URL {
             URL(string: "http://localhost:8080/api/products/user/\(userId)")!
